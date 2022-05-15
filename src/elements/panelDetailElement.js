987fdeac47cc0e6dc97b943d37ae7c6874602e47
Filip Vrba <filipvrba@pm.me>
2022-05-15 13:58:41 +0200

    hello

--- src/elements/panelDetailElement.js
+++ src/elements/panelDetailElement.js
@@ -98,14 +98,14 @@ class PanelDetailElement extends HTMLElement {
         }));
     }
 
-    // postMessage( data ) {
+    postMessage( data ) {
 
-    //     console.log( data );
-    // }
+        console.log( data );
+    }
 
     connectedCallback() {
 
-        // window.addEventListener( POST_MESSAGE, this.postMessageHandler );
+        window.addEventListener( POST_MESSAGE, this.postMessageHandler );
         document.addEventListener( PANEL, this.detailHandler );
 
         this.plus.addEventListener( CLICK, this.plusHandler );
@@ -114,7 +114,7 @@ class PanelDetailElement extends HTMLElement {
 
     disconnectedCallback() {
 
-        // window.removeEventListener( POST_MESSAGE, this.postMessageHandler );
+        window.removeEventListener( POST_MESSAGE, this.postMessageHandler );
         document.removeEventListener( PANEL, this.detailHandler );
 
         this.plus.removeEventListener( CLICK, this.plusHandler );
